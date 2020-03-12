5ce54efd59db435cf3f2b7b5d982e0a65ea260b4
saramortara <saramortara@gmail.com>
2020-03-12 11:13:51 -0300

    adding new output from script 3, now no duplicates

--- R/03_cleaning_missing_sp_data.R
+++ R/03_cleaning_missing_sp_data.R
@@ -1,6 +1,8 @@
 # Combining all records in a single table
 
 library(data.table)
+library(tools)
+library(dplyr)
 
 # loading files
 file_names <- list.files("results/splink_raw", full.names = TRUE)
@@ -14,16 +16,29 @@ full_data <- rbindlist(all_files, fill = TRUE)
 #nrow(full_data)
 
 # removing rows without complete species names
-clean_data <- full_data[!is.na(full_data$genus),]  # removing records with NA in genus
-clean_data <- clean_data[!is.na(clean_data$specificEpithet),] # removing records with NA in specificEpithet
-clean_data <- clean_data[which(clean_data$specificEpithet != "sp"),] # removing records with "sp" in specificEpithet
-clean_data <- clean_data[which(clean_data$specificEpithet != "sp."),] # removing  records with "sp." in specificEpithet
-clean_data$clean_id <- seq(1:nrow(clean_data)) # adding a new id
+# removing records with NA in genus or epithet
+clean_data <- full_data[!is.na(full_data$genus)
+                        & !is.na(full_data$specificEpithet), ]
+# removing records with "sp" or "sp." in specificEpithet
+clean_data <- clean_data[!clean_data$specificEpithet %in% c("sp", "sp."), ]
 
 # creating smaller table, with only taxonomic data
 # probably need more info here, but will come back later
-clean_taxon_data <- clean_data[, c("clean_id", "kingdom", "phylum", "order", "family", "genus", "specificEpithet", "scientificName", "scientificNameAuthorship")]
+clean_taxon_data <- clean_data[, c("kingdom", "phylum",
+                                   "order", "family",
+                                   "genus", "specificEpithet",
+                                   "scientificName", "scientificNameAuthorship")]
+
+# even selecting only plants, there are other kingdoms, selecting only plants
+plants <- clean_taxon_data[clean_taxon_data$kingdom %in% "Plantae", ]
+
+# excluding duplicated names
+plants_nodup <- plants[!duplicated(plants), ]
+plants_nodup$id_name <- paste("name",
+                              sprintf("%05d", 1:nrow(plants_nodup)),
+                              sep = "_")
 
 # write outputs
-write.csv(clean_data, file = "./results/clean_data.csv") # this generated a 104.6 MB file, exceeding github's limit
-write.csv(clean_taxon_data, file = "./results/clean_taxon_data.csv")
+write.csv(plants_nodup,
+          file = "./results/taxon_data_raw.csv",
+          row.names = FALSE)

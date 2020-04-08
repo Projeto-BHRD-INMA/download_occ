de551deba0bd407f960a8bcfe4e39119bed00bfe
saramortara <saramortara@gmail.com>
2020-04-08 17:17:17 -0300

    fix in check_status, new output to check

--- R/03_checking_strings.R
+++ R/03_checking_strings.R
@@ -10,28 +10,32 @@ library(rocc)
 # loading data
 tax <- read.csv("results/02_taxon_data_raw.csv")
 
+# 1. using rocc::check_status()
 check <- check_status(scientificName = tax$scientificName)
 
 table(check$scientificName_status)
 
-# vamos ficar com: possibly_ok, subspecies, variety, name_w_authors
-check$check_ok <- ifelse(check$scientificName_status
-                         %in% c("possibly_ok",
-                                "subspecies",
-                                "variety",
-                                "name_w_authors"),
-                         TRUE,
-                         FALSE)
+# vamos ficar com: possibly_ok, subspecies, variety, name_w_authors, name_w_non_ascii
+fica <- c("possibly_ok",
+          "subspecies",
+          "variety",
+          "name_w_authors",
+          "name_w_wrong_case")
+
+checar <- c("name_w_non_ascii",
+            "not_Genus_epithet_format",
+            "not_name_has_digits")
+
+check$check_ok <- "fora"
+check$check_ok[check$scientificName_status %in% fica] <- "ok"
+check$check_ok[check$scientificName_status %in% checar] <- "checar"
 
 # e agora juntando com o objeto inicial
 tax_check <- cbind(tax, check[, -1])
 
 head(tax_check)
 
-str(tax_check)
-
 # exportando os dados depois do check
-# write outputs
 write.csv(tax_check,
           file = "results/03_taxon_data_raw_check.csv",
           row.names = FALSE)

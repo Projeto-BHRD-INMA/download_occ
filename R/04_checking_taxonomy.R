643bc9d1427ceaeb23b33be0718ad420656ce2ef
saramortara <saramortara@gmail.com>
2020-04-09 18:02:02 -0300

    updating script 04 w/ new version of check_status

--- R/04_checking_taxonomy.R
+++ R/04_checking_taxonomy.R
@@ -6,12 +6,37 @@
 # 2. Kew - World Checklist of Vascular Plants
 # 3. TNRS - Taxonomic Name Resolution Service
 
-# Synonym check in flora's api
-## example for one species
-scientificName <- "Myrcia splendens"
-name <- gsub(" ", "%20", scientificName)
-api <- "http://servicos.jbrj.gov.br/flora/taxon/"
-res <- jsonlite::fromJSON(paste0(api, name))
+library(dplyr)
+library(flora)
+
+# reading file
+
+tax <- read.csv("results/03_taxon_data_raw_check.csv", as.is = TRUE)
+
+head(tax)
+
+tax_ok <- filter(tax, check_ok == "ok")
+tax_check <- filter(tax, check_ok == "checar")
+
+table(tax$check_ok)
+
+scientificName <- unique(tax_ok$scientificName_new)[1:20]
+#scientificName <- unique(tax_check$scientificName_new)
+
+length(scientificName)
+
+trim_sp <- flora::trim(scientificName)
+suggest_sp <- sapply(trim_sp, flora::suggest.names)
+
+search_flora <- function(x){
+  api <- "http://servicos.jbrj.gov.br/flora/taxon/"
+  search_sp <- gsub(" ", "%20", x)
+  res <- jsonlite::fromJSON(paste0(api, x))
+  return(res)
+}
+
+res_list <- lapply(suggest_sp, search_flora)
+
 if (!is.null(res$result$SINONIMO)) {
   sin.df <- res$result$SINONIMO[[1]]
 }

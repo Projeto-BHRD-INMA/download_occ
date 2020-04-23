b86a27ca30c43c66861306a59053f00e50016b03
saramortara <saramortara@gmail.com>
2020-04-23 20:42:35 -0300

    adding taxonomic check, now 13781 species

--- R/03_checking_strings.R
+++ R/03_checking_strings.R
@@ -6,11 +6,12 @@
 
 # loading packages
 library(rocc)
+library(dplyr)
 
 # loading data
 tax <- read.csv("results/02_taxon_data_raw.csv")
 
-# 1. using rocc::check_string()
+# 1. using rocc::check_string() ####
 check <- check_string(scientificName = tax$scientificName)
 
 table(check$scientificName_status)
@@ -26,14 +27,28 @@ checar <- c("name_w_non_ascii",
             "not_Genus_epithet_format",
             "not_name_has_digits")
 
+# 2. checking fields ####
+## name_w_non_ascii
+## remove Não identificada
+tax %>% filter(check$scientificName_status %in% checar[1])
+
+## not_Genus_epithet_format
+## too much
+#tax %>% filter(check$scientificName_status %in% checar[2])
+
+## not_name_has_digits
+tax %>% filter(check$scientificName_status %in% checar[3]) # ok
+
 check$check_ok <- "fora"
-check$check_ok[check$scientificName_status %in% fica] <- "ok"
-check$check_ok[check$scientificName_status %in% checar] <- "checar"
+check$check_ok[check$scientificName_status %in% c(fica, checar[-2])] <- "ok"
+check$check_ok[check$scientificName_status %in% checar[1] &
+                 check$scientificName == "Não identificada"] <- "fora"
+check$check_ok[check$scientificName_status %in% "not_Genus_epithet_format"] <- "checar"
+
+table(check$check_ok)
 
 # e agora juntando com o objeto inicial
 tax_check <- cbind(tax, check[, -1])
-
-head(tax_check)
 
 # exportando os dados depois do check
 write.csv(tax_check,
--- R/04_checking_taxonomy.R
+++ R/04_checking_taxonomy.R
@@ -9,19 +9,65 @@
 library(dplyr)
 library(flora)
 library(rocc)
+library(parallel)
 
 # reading file
-
 tax <- read.csv("results/03_taxon_data_raw_check.csv", as.is = TRUE)
 
 head(tax)
 
-tax_ok <- filter(tax, check_ok == "ok")
-tax_check <- filter(tax, check_ok == "checar")
+tax_ok <- tax %>% filter(check_ok == "ok")
 
-table(tax$check_ok)
+scientificName <- unique(tax_ok$scientificName_new)
+length(scientificName)
 
-scientificName <- unique(tax_ok$scientificName_new)[1]
-#scientificName <- unique(tax_check$scientificName_new)
+# 1. suggesting a name ####
+# using parallel
 
-teste <- suggest_bfg(scientificName)
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+# Initiate cluster
+cl <- makeCluster(no_cores)
+
+start_time <- Sys.time()
+list_taxa <- parLapply(cl, scientificName, suggest_flora)
+end_time <- Sys.time()
+
+t1 <- end_time - start_time
+
+stopCluster(cl)
+
+suggest_taxa <- bind_rows(list_taxa)
+
+search_taxa <- suggest_taxa$scientificName_suggest %>%
+  unique() %>%
+  na.omit()
+
+search_df <- data.frame(scientificName_search = search_taxa,
+                        search_id = 1:length(search_taxa))
+# 2. checking if the name exists in Brazilian Flora ####
+
+flora_taxa <- list()
+for (i in 1:length(search_taxa)) {
+  message(paste(i, "species"))
+  flora_taxa[[i]] <- check_flora(search_taxa[i],
+                                 get_synonyms = FALSE,
+                                 infraspecies = TRUE)
+}
+
+length(flora_taxa)
+
+flora_taxa2 <- lapply(flora_taxa, function(x) x[1]$taxon)
+
+flora_df <- bind_rows(flora_taxa2)
+
+head(flora_df)
+
+table(flora_df$taxonomicstatus)
+
+flora_df2 <- left_join(flora_df, search_df, by = "scientificName_search")
+
+# writing output
+write.csv(flora_df2,
+          file = "results/04_taxon_data_flora_check.csv",
+          row.names = FALSE)
